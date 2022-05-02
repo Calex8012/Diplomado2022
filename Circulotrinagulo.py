a5e7b19ac8504463ca29fab565df0b3ccb50592e
Calex8012 <42974499+Calex8012@users.noreply.github.com>
2022-05-01 19:38:18 -0500

    Update Circulotrinagulo.py

--- Circulotrinagulo.py
+++ Circulotrinagulo.py
@@ -5,7 +5,6 @@ b = int(input("Insert side 'b':"))
 c = int(input("Insert side 'c':"))
 
 s = (a+b+c)*1/2
-
+print("El semiperimetro es = ",s)
 r = math.sqrt(s*(s-a)*(s-b)*(s-c))/s
-
-print(r)
+print("El radio de la circunferencia inscrita en un triangulo es =",r)
