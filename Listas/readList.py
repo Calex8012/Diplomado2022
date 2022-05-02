8cca4f9e5f308c6ec471166275124d193e687781
Calex8012 <42974499+Calex8012@users.noreply.github.com>
2022-05-01 22:09:26 -0500

    Update readList.py

--- Listas/readList.py
+++ Listas/readList.py
@@ -11,9 +11,7 @@ with open('lista.bin','rb') as fh:
     print("La suma es de:",suma,"\n")
     
     avg = statistics.mean(ListRead) 
-    print("El promedio de este conjunto de numeros aleatorios es:"
-          ,avg,"\n") 
+    print("El promedio es:",avg,"\n") 
     
     moda = statistics.mode(ListRead) 
-    print("La moda de este conjunto de numeros aleatorios es:"
-          ,moda,"\n")
+    print("La moda es:",moda,"\n")
