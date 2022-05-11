ba48212fe5f2ab049e26a379fbbb2486208c12c1
Calex8012 <42974499+Calex8012@users.noreply.github.com>
2022-05-11 10:59:00 -0500

    Update 4roots.py

--- 4roots/4roots.py
+++ 4roots/4roots.py
@@ -1,18 +1,20 @@
+#Obtener la raiz de e^{2-x}-7=0
+from math import exp
 
-from math import sin,cos
-def f(x):
-    return 2-x+sin(2*x)
-def f2(x):
-    return -1+2*cos(2*x)
+def fun(x):
+ return exp(2-x)-7*x
+def der(x):
+ return -exp(2-x)-7
 
-x0=1.5
-x1=x0-f(x0)/f2(x0)
-print('x1= %.6f '%x1)
+x0=1
+x1=x0-fun(x0)/der(x0)
+print('x1= ', x1) 
 
-x2=x1-f(x1)/f2(x1)
-print('x2=',x2)
+x2=x1-fun(x1)/der(x1)
+print('x2= ', x2)
 
-x3=x2-f(x2)/f2(x2)
-print('x3=',x3)
-print('valor=',f(x3))
-print('La raíz es ',x3)
+x3=x2-fun(x2)/der(x2)
+print('Valor de x3= ', x3)
+print('Valor de la funcion =',fun(x3))
+print('La raiz es ',x3)
+ #No supe como graficarla la raiz 
