cc1eff4ddef53cd1dba1e4999dcef18f964faffb
Calex8012 <42974499+Calex8012@users.noreply.github.com>
2022-05-15 15:37:42 -0500

    Delete Diccionary directory

--- Diccionary/readDict2do.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import pprint
-
-with open('Dictionary.bin','rb') as fh:
-    Dict=pickle.load(fh)
-    
-    
-    print(Dict)
-    print(type(Dict))
--- Diccionary/writeDict2do.py
+++
@@ -1,9 +0,0 @@
-import pickle
-from typing import Dict
-
-MovDev = {'Abeja' , 'Zapatos', 'Ropa', }
-
-with open ('Dictionary.bin','wb') as fh:
-    pickle.dump(Dict,fh)
-    
-    print("Archivo binario creado")
